85af394c3e92a9b56f1aa6d40a2c389b8f20b57c
mikej <mike@mikejenkins.co.uk>
2025-02-07 11:33:09 +0000

    Added unit test for API

---
+++ tests/unit-tests/test_main.py
@@ -0,0 +1,27 @@
+from unittest.mock import Mock
+
+import pytest
+from fastapi import HTTPException
+
+from main import get_task
+from model.models import Task
+from service.exceptions import TaskNotFoundException
+from service.task_service import TaskService
+
+
+def test_should_get_task_by_id():
+    mock_service = Mock()
+    mock_service.get_task.return_value = Task(id=1, title='Test Task', status='Todo')
+
+    task: Task = get_task(1, mock_service)
+    assert task.id == 1
+
+
+def test_should_return_http_not_found_error_when_task_doesnt_exist():
+    mock_service = Mock()
+    mock_service.get_task.side_effect = TaskNotFoundException('Task not found')
+
+    with pytest.raises(HTTPException) as err:
+        get_task(1, mock_service)
+
+    assert err.value.status_code == 404
